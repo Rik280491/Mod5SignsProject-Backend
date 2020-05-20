a8389c4dea1546b6b004c95ab96193f3c2615eb3
RikBala <rik280491@googlemail.com>
2020-05-20 23:01:58 +0100

    error handling for upload

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -16,7 +16,8 @@ class SignsController < ApplicationController
           video = get_user.videos.build(sign_id: sign.id, video_url: params[:video_url])
           video.save
           
-          render json: sign, include: [:videos]
+          # render json: sign, include: [:videos]
+          render json: { message: "Your video has been uploaded. Thank you for your contribution!"}
         else 
           render json: { error: "Sign must have a name" }
         end
